f22c7936e8f8295506489fb36fe6f1d711157dc0
Imanelakehal <skyrelia7@gmail.com>
2026-02-05 00:23:04 +0000

    Deleting flavors

--- lib/main_dev.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'main_common.dart';
-import 'app_config.dart';
-
-void main() async {
-  AppConfig.initialize(Flavor.dev); // dev flavor
-  await mainCommon();
-}
--- lib/main_prod.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'main_common.dart';
-import 'app_config.dart';
-
-void main() async {
-  AppConfig.initialize(Flavor.prod); // prod flavor
-  await mainCommon();
-}
